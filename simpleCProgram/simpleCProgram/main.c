88c76697a23d146d0f98af620c87b5c95158591d
mac <mac@macs-mbp-5.india.xoriantcorp.com>
2015-11-28 02:33:56 +0530

    variables c

---
+++ simpleCProgram/simpleCProgram/main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  simpleCProgram
+//
+//  Created by mac on 28/11/15.
+//  Copyright © 2015 Xoriant. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int days_in_a_week = 23;
+    float cm_to_in = 2.54;
+    
+    char the_w = 'W';
+    printf("%d days in a week. \n", days_in_a_week);
+    printf("%f centimeter to inch. \n", cm_to_in);
+    printf("%c capital W. \n", the_w);
+    
+    
+    printf("Hello, World!\n");
+    return 0;
+}
