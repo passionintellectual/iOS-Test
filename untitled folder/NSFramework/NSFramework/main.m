70fe119ad240197002f2013f0037aa2ab35f1afc
mac <mac@macs-mbp-5.india.xoriantcorp.com>
2015-11-28 17:30:55 +0530

    NSNumber

---
+++ untitled folder/NSFramework/NSFramework/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  NSFramework
+//
+//  Created by mac on 28/11/15.
+//  Copyright © 2015 Xoriant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSNumber *asd = [NSNumber numberWithFloat:12.234];
+        NSLog(@"asd %@\n", asd);
+        NSNumber *in = @65;
+        NSLog(@"in %@\n", in);
+    }
+    return 0;
+}
